01a642d750cc1a3a415bef0cc321c7ac2f1a9c32
teamjaf <65031312+teamjaf@users.noreply.github.com>
2021-12-21 10:44:12 +0000

    log in synced with room page

---
+++ src/main/java/com/example/Login/LoginRepository.java
@@ -0,0 +1,11 @@
+package com.example.Login;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoginRepository extends JpaRepository<Login, Long> {
+    Login findByUsernameAndPassword(String username, String password);
+
+}
---
+++ src/main/java/com/example/Login/Loginservice.java
@@ -0,0 +1,18 @@
+package com.example.Login;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+class LoginService {
+    @Autowired
+    private LoginRepository repo;
+
+    public Login login(String username, String password) {
+        Login user = repo.findByUsernameAndPassword(username, password);
+        return user;
+    }
+
+}
